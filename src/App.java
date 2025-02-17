202da4127d837a90d77a4ac2e0a4ea28e3768392
PTriet <nguyentriet0112@gmail.com>
2025-02-18 00:08:21 +0700

    push code

---
+++ src/App.java
@@ -0,0 +1,24 @@
+import javax.swing.JFrame;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int rowCount = 21;
+        int colCount = 19;
+        int tileSize = 32;
+        int boardWidth = colCount * tileSize;
+        int boardHeight = rowCount * tileSize;
+        JFrame frame = new JFrame("Pacman");
+        // frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        PacMan pacmanGame = new PacMan();
+        frame.add(pacmanGame);
+        frame.pack();
+        pacmanGame.requestFocus();
+        frame.setVisible(true);
+
+    }
+}
